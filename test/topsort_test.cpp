04b2d2cd7dad10db1c3ef6a86f8c07d1cb6290df
gilligk0 <s3kagill@uni-bonn.de>
2025-02-24 15:07:19 +0100

    added topsort_test instance. However this is somehov not linking correctly yet

---
+++ test/topsort_test.cpp
@@ -0,0 +1,12 @@
+// Created by kathy on 24.02.25.
+//
+
+#include <gtest/gtest.h>
+#include <graphgenerator.hpp>
+#include "graph.hpp"
+
+
+
+TEST(ExampleTests, reallyBasicTest) {
+    EXPECT_EQ(1, 1);
+}
